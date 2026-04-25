0cec369819f5ebae42206c752387bc53c4c71d21
GittingNemo <r1074198@student.thomasmore.be>
2026-04-25 20:49:56 +0200

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FinancyApplication
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+
+}
